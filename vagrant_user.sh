e28566a5640b2a5cc6821b67b5933fda7e91e1bf
Shitikov Kyrylo <kshy@isddesign.com>
2017-12-25 22:51:01 +0200

    Change Vagrant file

--- vagrant_user.sh
+++ vagrant_user.sh
@@ -8,38 +8,38 @@ echo "Config file: $CONFIG_FILE"
 echo "Project path: $PROJECT_PATH"
 echo "-------------------------------------------------"
 
-echo "Install composer"
+echo "---- Install composer ----"
 cd $PROJECT_PATH
 composer install
 
-echo "Install NVM"
+echo "---- Install NVM ----"
 sudo apt-get install build-essential libssl-dev
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 
-cat <<EOF >> ~/.bash_profile
+cat >> ~/.profile <<EOF
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 EOF
 
-source ~/.bash_profile
+sudo su - $USER
+. ~/.profile
 
-npm_path=$(command -v nvm)
-echo "NPM path: $npm_path"
+#npm_path=$(command -v nvm)
+echo "---- NPM path: $NVM_DIR ----"
 
-echo "Install NODE"
+echo "---- Install NODE ----"
 nvm install node
 node install
 
-echo "Install bower"
+echo "---- Install bower ----"
 npm install -g bower
 
 bower_file="bower.json"
 if [ -f "$bower_file" ]
 then
-    echo "Install bower dependency"
+    echo "---- Install bower dependency ----"
     bower install
 else
-    echo "No bower.json present"
+    echo "---- No bower.json present ----"
 fi
-
