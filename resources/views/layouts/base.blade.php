a638e2419b5571575b63c1bbf05af09ee8687b5f
Shitikov Kyrylo <kshy@isddesign.com>
2017-12-25 23:47:47 +0200

    Fix template

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,27 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>
-        @section('title')
-            Material Admin - Blank page
-        @show
-    </title>
+    <title>@section('title')Material Admin - Blank page@show</title>
 
     <!-- BEGIN META -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @section('meta')
-        <meta name="keywords" content="your,keywords">
-        <meta name="description" content="Short explanation about this website">
+    <meta name="keywords" content="your,keywords">
+    <meta name="description" content="Short explanation about this website">
     @show
-<!-- END META -->
+    <!-- END META -->
 
-    <!-- BEGIN STYLESHEETS -->
-    @section('style')
-        <link href='http://fonts.googleapis.com/css?family=Roboto:300italic,400italic,300,400,500,700,900' rel='stylesheet' type='text/css'/>
-        <link type="text/css" rel="stylesheet" href="{{ asset(env('THEME')) }}/css/theme-default/style.css?1425466319" />
-        <link type="text/css" rel="stylesheet" href="{{ asset(env('THEME')) }}/css/theme-default/font-awesome.min.css?1422529194" />
-        <link type="text/css" rel="stylesheet" href="{{ asset(env('THEME')) }}/css/theme-default/material-design-iconic-font.min.css?1421434286" />
+<!-- BEGIN STYLESHEETS -->
+@section('style')
+    <link href='http://fonts.googleapis.com/css?family=Roboto:300italic,400italic,300,400,500,700,900' rel='stylesheet' type='text/css'/>
+    <link type="text/css" rel="stylesheet" href="{{ asset(env('THEME')) }}/css/theme-default/style.css?1425466319" />
+    <link type="text/css" rel="stylesheet" href="{{ asset(env('THEME')) }}/css/theme-default/font-awesome.min.css?1422529194" />
+    <link type="text/css" rel="stylesheet" href="{{ asset(env('THEME')) }}/css/theme-default/material-design-iconic-font.min.css?1421434286" />
 @show
 <!-- END STYLESHEETS -->
 
